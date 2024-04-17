f87c47341c0dd2f77072e84c3941f131784c8334
alirionx <dquilitzsch@outlook.de>
2024-04-18 00:07:32 +0200

    sac

--- hello-api/app.py
+++ hello-api/app.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     @app.route('/')
     def hello_api():
         return main()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
 
 #------------------------------------------------------
